3287ff8dbed86337d1102e90b96e74c6449f068a
suvakantamalik <134865642+suvakantamalik@users.noreply.github.com>
2024-09-01 18:35:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //your JS code here. If required.
+
 const btn = document.getElementById("submit");
 const list = document.getElementById("book-list");
 btn.addEventListener("click", () => {
@@ -11,14 +12,29 @@ btn.addEventListener("click", () => {
 	let td1 = document.createElement("td");
 	let td2 = document.createElement("td");
 	let td3 = document.createElement("td");
+	let td4 = document.createElement("td");
  
 	td1.innerText = title;
 	td2.innerText = author;
 	td3.innerText = isbn;
 
+	let closeBtn = document.createElement("span");
+	closeBtn.innerHTML = '<i class="fa-solid fa-rectangle-xmark"></i>';
+	closeBtn.style.color = "red";
+	closeBtn.style.cursor = "pointer";
+    closeBtn.style.padding = "5px";
+    closeBtn.style.borderRadius = "3px";
+    closeBtn.style.display = "inline-block";
+	closeBtn.addEventListener("click", () => {
+		tr.remove();
+	})
+	
+	td4.appendChild(closeBtn);
+
 	tr.appendChild(td1);	
 	tr.appendChild(td2);
 	tr.appendChild(td3);
+	tr.appendChild(td4);
 
 	list.appendChild(tr);
 });
